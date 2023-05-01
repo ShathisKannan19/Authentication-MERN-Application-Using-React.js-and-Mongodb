358dcacc048edb5776eba25e3abc4761914342bd
ShathisKannan19 <110104103+ShathisKannan19@users.noreply.github.com>
2023-05-01 12:04:37 +0530

    Update index.jsx

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -15,7 +15,7 @@ const Login = () => {
 		console.log(data);
 		e.preventDefault();
 		try {
-			const url = "http://localhost:5000/api/auth";
+			const url = "https://react-shan.onrender.com/api/auth";
 			const { data: res } = await axios.post(url, data);
 			localStorage.setItem("token", res.data);
 			window.location = "/";
