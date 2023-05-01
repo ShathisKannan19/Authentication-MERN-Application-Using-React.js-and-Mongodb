b206e9c8c64445ff3441e564b23616a60f596fbf
ShathisKannan19 <110104103+ShathisKannan19@users.noreply.github.com>
2023-05-01 15:10:34 +0530

    Update index.jsx

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -56,7 +56,7 @@ const Login = () => {
 						/>
 						{error && <div className={styles.error_msg}>{error}</div>}
 						<button type="submit" className={styles.green_btn}>
-							Sing In
+							Sign In
 						</button>
 					</form>
 				</div>
@@ -64,7 +64,7 @@ const Login = () => {
 					<h1>New Here ?</h1>
 					<Link to="/signup">
 						<button type="button" className={styles.white_btn}>
-							Sing Up
+							Sign Up
 						</button>
 					</Link>
 				</div>
